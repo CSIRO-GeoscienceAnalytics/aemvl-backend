693c5dda5e9508134d3b558867afc00cc2a46313
Adam Brown <adam.brown@evbane.com>
2017-07-05 11:14:05 +0800

    Updated sample file

--- tests/postman/curl_test.sh
+++ tests/postman/curl_test.sh
@@ -4,7 +4,7 @@ echo '' > cookies.txt
 
 curl \
     -c cookies.txt \
-    -F "datafile=@../../docs/ExampleTempestData.From.Client.xyz" \
+    -F "datafile=@../../docs/AUS_10004_CSIRO_EM_HM_reduced.XYZ" \
     -F "configfile=@../../docs/AUS_10004_CSIRO_SkyTem_EM.json" \
     -F "project_id=TESTPROJECT" \
     -H "Accept: text/csv" \
@@ -20,7 +20,7 @@ curl \
 curl \
     -b cookies.txt \
     -c cookies.txt \
-    -F "line_number=10030" \
+    -F "line_number=200301" \
     -F "column_names=HM_Z,PLNI" \
     -F "project_id=TESTPROJECT" \
     -H "Accept: text/csv" \
